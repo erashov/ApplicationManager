6b8f85d33fe0c4ab41df55631ae2e7ed6252834b
Alex <erashov@outlook.com>
2017-06-19 16:51:29 +0300

    add fronend envorinment

---
+++ ApplicationManagerSPA/ClientApp/app/components/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewChild } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  tiles = [
+    { text: 'One', cols: 3, rows: 1, color: 'red' },
+    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+  ];
+}
