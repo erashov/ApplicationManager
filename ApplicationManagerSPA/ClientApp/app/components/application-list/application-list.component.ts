2b24acb0c846ae98ddd79afe7911ee74f17dfedc
Alex <erashov@outlook.com>
2017-06-19 16:53:37 +0300

    add sign-in

---
+++ ApplicationManagerSPA/ClientApp/app/components/application-list/application-list.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'application-list',
+    templateUrl: './application-list.component.html',
+    styleUrls:['./application-list.component.css']
+})
+
+export class ApplicationListComponent {
+   
+}
